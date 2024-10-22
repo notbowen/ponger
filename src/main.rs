fd6441bcdc4a14c6cf9af36cd3d56d107d89395b
Hu Bowen <contact@hubowen.dev>
2024-10-22 19:58:57 +0800

    feat: clean up code

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,13 @@ use chrono::{self, DateTime};
 use poise::serenity_prelude as serenity;
 use poise::serenity_prelude::CreateMessage;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
-use poise::Modal;
+use rand::RngCore;
 use serde::Deserialize;
 use shuttle_runtime::SecretStore;
 use shuttle_serenity::ShuttleSerenity;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-#[derive(Debug, Modal)]
-#[name = "CTF Details"]
-struct CtfDetailModal {
-    #[name = "Name of CTF"]
-    #[placeholder = "GreyCTF 2024"]
-    name: String,
-    #[name = "Start of CTF"]
-    #[placeholder = "20/04/2024 12:00 +0800"]
-    start_date: String,
-    #[name = "End of CTF"]
-    #[placeholder = "21/04/2024 12:00 +0800"]
-    end_date: String,
-    #[name = "Link to CTF Platform"]
-    #[placeholder = "https://ctf.nusgreyhats.org/"]
-    url: String,
-    #[name = "CTFTime URL"]
-    #[placeholder = "https://ctftime.org/event/2242"]
-    ctftime_url: Option<String>,
-}
 
 #[derive(Debug, Deserialize)]
 struct CtfTimeResponse {
@@ -39,15 +18,24 @@ struct CtfTimeResponse {
     start: String,
     finish: String,
     url: String,
+    logo: String,
+    description: String,
+}
+
+fn random_color() -> (u8, u8, u8) {
+    let mut bytes = [0_u8; 3];
+    rand::thread_rng().fill_bytes(&mut bytes);
+    (bytes[0], bytes[1], bytes[2])
 }
 
 #[poise::command(slash_command)]
-async fn grab_ctf_details(
+async fn send_ctf(
     ctx: poise::ApplicationContext<'_, Data, Error>,
     #[description = "Channel ID"] channel_id: serenity::ChannelId,
     #[description = "CTFTime URL"] url: String,
 ) -> Result<(), Error> {
-    let response = reqwest::get(&format!("https://ctftime.org/api/v1/events/{}/", url))
+    let id = url.split('/').last().unwrap();
+    let response = reqwest::get(&format!("https://ctftime.org/api/v1/events/{}/", id))
         .await?
         .json::<CtfTimeResponse>()
         .await?;
@@ -58,57 +46,25 @@ async fn grab_ctf_details(
     let embed = serenity::CreateEmbed::default()
         .title(response.title)
         .description(format!(
-            "Start: {}\nEnd: {}\nURL: {}\nCTFTime: {}",
+            "**Start**: {}\n**End**: {}\n\n**URL**: {}\n\n**Description**:\n{}",
             format!("<t:{}:R>", start_timestamp.unwrap().timestamp()),
             format!("<t:{}:R>", end_timestamp.unwrap().timestamp()),
             response.url,
-            response.ctftime_url
+            response.description,
         ))
-        .color(0x00ff00);
+        .thumbnail(if response.logo.is_empty() {
+            "https://ctftime.org/static/images/nologo.png".to_string()
+        } else {
+            response.logo
+        })
+        .url(response.ctftime_url)
+        .color(random_color());
 
     channel_id
         .send_message(&ctx.http(), CreateMessage::new().embed(embed))
         .await?;
 
     ctx.say(":white_check_mark: Sent!").await?;
-    Ok(())
-}
-
-#[poise::command(slash_command)]
-async fn send_ctf_details(
-    ctx: poise::ApplicationContext<'_, Data, Error>,
-    #[description = "Channel ID"] channel_id: serenity::ChannelId,
-) -> Result<(), Error> {
-    let data = match CtfDetailModal::execute(ctx).await? {
-        Some(data) => data,
-        None => {
-            ctx.say("Could not get CTF details").await?;
-            return Ok(());
-        }
-    };
-
-    let start_timestamp = DateTime::parse_from_str(&data.start_date, "%d/%m/%Y %H:%M %z");
-    let end_timestamp = DateTime::parse_from_str(&data.end_date, "%d/%m/%Y %H:%M %z");
-
-    let embed = serenity::CreateEmbed::default()
-        .title(data.name)
-        .description(format!(
-            "Start: {}\nEnd: {}\nURL: {}{}",
-            format!("<t:{}:R>", start_timestamp.unwrap().timestamp()),
-            format!("<t:{}:R>", end_timestamp.unwrap().timestamp()),
-            data.url,
-            {
-                match data.ctftime_url {
-                    Some(url) => format!("\n**CTFTime**: {}", url),
-                    None => "".to_string(),
-                }
-            }
-        ))
-        .color(0x00ff00);
-
-    channel_id
-        .send_message(&ctx.http(), CreateMessage::new().embed(embed))
-        .await?;
     Ok(())
 }
 
@@ -120,7 +76,7 @@ async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleS
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![grab_ctf_details(), send_ctf_details()],
+            commands: vec![send_ctf()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
